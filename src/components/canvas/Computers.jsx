3f2f95e4ece9c87fbaceb9b93c9e60a02b6ca2bd
Nitish Ahuja <nitishahuja971@gmail.com>
2023-12-31 06:22:05 -0500

    changes in structue

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("src/assets/desktop_pc/scene.gltf");
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
