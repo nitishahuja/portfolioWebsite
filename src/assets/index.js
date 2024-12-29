455fa2dd3bd557310460ca76dd9a0890c9f13516
Nitish Ahuja <nitishahuja971@gmail.com>
2024-12-28 20:06:42 -0500

    added

--- src/assets/index.js
+++ src/assets/index.js
@@ -12,6 +12,7 @@ import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
+
 import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
